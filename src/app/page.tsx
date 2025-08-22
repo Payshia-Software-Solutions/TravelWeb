654404f21fb31ff40466b13ef6d9b8cb2339715c
Dulaj Hansana <dulajhansana1973@gmail.com>
2025-08-22 09:07:10 +0000

    remove this two button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -88,8 +88,6 @@ export default function Home() {
                         </CarouselItem>
                     ))}
                 </CarouselContent>
-                <CarouselPrevious className="absolute left-4 top-1/2 -translate-y-1/2 hidden md:flex" />
-                <CarouselNext className="absolute right-4 top-1/2 -translate-y-1/2 hidden md:flex" />
             </Carousel>
             <div className="absolute inset-0 bg-black/50 z-10" />
             <div className="absolute z-30 left-4 top-1/2 -translate-y-1/2 flex flex-col gap-2">
